ff09d828af3e3e7e357d205e678f75f466ce801f
Kimsuro <ss01056489656@gmail.com>
2026-01-17 22:39:33 +0900

    refactor: 모든 속성이 전달 가능하게 Input Components 수정

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,14 +1,20 @@
-interface InputProps {
+import { InputHTMLAttributes } from "react";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   type: string;
   placeholder?: string;
 }
 
-export default function Input({ type, placeholder }: InputProps) {
+export default function Input({ type, placeholder, className = "", ...props }: InputProps) {
+  const baseStyles =
+    "py-4 px-6 rounded-3xl border-slate-900 border-2 w-full text-slate-900 bg-slate-100 shadow-[4px_3.5px_0_0_rgb(15_23_42)] outline-none";
+
   return (
     <input
       type={type}
       placeholder={placeholder}
-      className="py-4 px-6 rounded-3xl border-slate-900 border-2 w-full text-slate-900 bg-slate-100 shadow-[4px_3.5px_0_0_rgb(15_23_42)] outline-none"
-    ></input>
+      className={`${baseStyles} ${className}`}
+      {...props}
+    />
   );
 }
